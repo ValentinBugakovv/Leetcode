44a58fbaa9a81ca9d7a8cdfe619dfdd12f3102a6
Valentin Bugakov <33461703+ValentinBugakovv@users.noreply.github.com>
2025-01-08 17:58:25 +0300

    Create valid_palindrome.py

---
+++ valid_palindrome.py
@@ -0,0 +1,16 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = re.sub(r'[^\w\s]', '', s).lower().replace(' ', '')
+      #  print(s)
+        left = 0
+        right = len(s) - 1
+        while left != right:
+       #     print(s[left], s[right])
+            if s[left] != s[right]:
+                return False
+            else:
+                left += 1
+                right -= 1
+        return True 
